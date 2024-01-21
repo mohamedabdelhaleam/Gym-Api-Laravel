975696f9d3831aa910368a62d8593cdc20345312
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-01-21 16:26:36 +0200

    add Relations to Subscription model

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -11,4 +11,15 @@ class Subscription extends Model
     protected $table = "subscriptions";
     protected $fillable = ['program_id', 'user_id'];
     protected $hidden = ['program_id', 'user_id'];
+    protected $timestamps = true;
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+    public function Programs()
+    {
+        return $this->belongsTo(User::class, 'program_id', 'id');
+    }
 }
