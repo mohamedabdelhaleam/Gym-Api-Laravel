44403fe70c713d132fadfcbe0b330b986d3644cd
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-01-23 16:41:40 +0200

    Create Program request

---
+++ app/Http/Requests/ProgramRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProgramRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required | min:5 | max:100',
+            'coach_id' => 'required | min:5 | max:100',
+            'price' => 'required',
+        ];
+    }
+}
